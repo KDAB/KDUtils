acbfd7766c72eaff64d431bf10d8053f3e9bf437
Joshua Goins <joshua.goins@kdab.com>
2023-07-06 11:52:53 -0400

    Add config.h for KDGui to detect platform backend support downstream
    You can detect the compiled platform for KDFoundation, but there's no
    equivalent for consumers of KDGui. This gets more complex, for example
    KDGui on Linux could have two backends: XCB and Wayland. We need to be
    able to detect which one is built.
    
    Change-Id: I7b5aba25fb795edd202da8934c6b96e59dff2ea9
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdutils/+/127780
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

---
+++ src/KDGui/config.h
@@ -0,0 +1,20 @@
+/*
+    This file is part of KDUtils.
+
+    SPDX-FileCopyrightText: 2018-2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+    Author: Joshua Goins <joshua.goins@kdab.com>
+
+    SPDX-License-Identifier: MIT
+
+    Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#pragma once
+
+// clang-format off
+#cmakedefine KDGUI_PLATFORM_ANDROID
+#cmakedefine KDGUI_PLATFORM_COCOA
+#cmakedefine KDGUI_PLATFORM_XCB
+#cmakedefine KDGUI_PLATFORM_WAYLAND
+#cmakedefine KDGUI_PLATFORM_WIN32
+// clang-format on
