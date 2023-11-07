03f7602a7f77440fa5efed94220935f6fd44a596
Joshua Goins <joshua.goins@kdab.com>
2023-11-07 17:13:07 -0500

    Add some tests for Position
    Adds a bunch of test cases for Position, including one that was
    previously a bug in the not equal operator.
    
    Change-Id: If49c035843d562fbff30f0da37a37fb67d5b020b
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdutils/+/133342
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>

---
+++ tests/auto/gui/position/tst_position.cpp
@@ -0,0 +1,47 @@
+/*
+    This file is part of KDUtils.
+
+    SPDX-FileCopyrightText: 2021-2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+    Author: Joshua Goins <joshua.goins@kdab.com>
+
+    SPDX-License-Identifier: MIT
+
+    Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include <KDGui/position.h>
+#include <array>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+using namespace KDGui;
+
+TEST_SUITE("Position")
+{
+    TEST_CASE("Constructor")
+    {
+        const Position pos{ 1, 2 };
+        REQUIRE_EQ(pos.x, 1);
+        REQUIRE_EQ(pos.y, 2);
+    }
+    TEST_CASE("Equality")
+    {
+        const Position a{ 0, 2 };
+        const Position b{ 3, 4 };
+        const Position c{ 0, 0 };
+
+        REQUIRE(a == a);
+        REQUIRE(a != b);
+        REQUIRE(a != c);
+    }
+    TEST_CASE("Math Operators")
+    {
+        const Position a{ 1, 2 };
+        const Position b{ 3, 4 };
+
+        REQUIRE_EQ(a + b, Position(4, 6));
+        REQUIRE_EQ(a - b, Position(-2, -2));
+        REQUIRE_EQ(a / int64_t{ 2 }, Position(0, 1));
+    }
+}
