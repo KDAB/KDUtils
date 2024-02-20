95b671c7a43bbd490d4f198ba91320022f6f3f6a
Shivam Kunwar <shivam.kunwar@kdab.com>
2024-02-20 13:36:04 +0530

    extend the test cases

--- tests/auto/utils/signal/tst_signal.cpp
+++ tests/auto/utils/signal/tst_signal.cpp
@@ -40,4 +40,63 @@ TEST_SUITE("Signal")
 
         CHECK(val == 9);
     }
+
+    TEST_CASE("Emit Multiple Signals with Evaluator")
+    {
+        auto *appInstance = new KDFoundation::CoreApplication;
+        KDUtils::Signal<int> signal1;
+        KDUtils::Signal<int> signal2;
+
+        int val1 = 4;
+        int val2 = 4;
+
+        signal1.connect(appInstance->connectionEvaluator(), [&val1](int value) {
+            val1 += value;
+        });
+
+        signal2.connect(appInstance->connectionEvaluator(), [&val2](int value) {
+            val2 += value;
+        });
+
+        std::thread thread1([&] {
+            signal1.emit(2);
+        });
+
+        std::thread thread2([&] {
+            signal2.emit(3);
+        });
+
+        thread1.join();
+        thread2.join();
+
+        CHECK(val1 == 4);
+        CHECK(val2 == 4);
+
+        appInstance->connectionEvaluator()->evaluateDeferredConnections();
+
+        CHECK(val1 == 6);
+        CHECK(val2 == 7);
+    }
+
+    TEST_CASE("Connect, Emit, Disconnect, and Evaluate")
+    {
+        auto *appInstance = new KDFoundation::CoreApplication;
+        KDUtils::Signal<int> signal;
+        int val = 4;
+
+        auto connection = signal.connect(appInstance->connectionEvaluator(), [&val](int value) {
+            val += value;
+        });
+
+        CHECK(connection.isActive());
+
+        signal.emit(2);
+        CHECK(val == 4);
+
+        connection.disconnect();
+
+        appInstance->connectionEvaluator()->evaluateDeferredConnections();
+
+        CHECK(val == 4);
+    }
 }
