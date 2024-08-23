67927d24353001b78a87dc77893be1bbfe6e0fff
Miłosz Kosobucki <milosz.kosobucki@kdab.com>
2024-08-23 14:42:17 +0200

    Switch pre-commit to markdownlint-cli2
    It doesn't require Ruby and works out of the box on Windows. Original
    markdownlint wasn't able to install Ruby properly on Windows.

--- .mdlrc.rb
+++
@@ -1,5 +0,0 @@
-all
-rule 'MD007', :indent => 2, :start_indented => false
-rule 'MD013', :line_length => 100, :tables => false, :ignore_code_blocks => true
-rule 'MD029', :style => :ordered
-exclude_rule 'MD033'
