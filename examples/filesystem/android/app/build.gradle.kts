baaacbb059b821cdcab750bdf464665d55b9dcb6
Alistair Baxter <alistair.baxter@kdab.com>
2025-03-19 17:43:41 +0000

    Add an example that exercises filesystem functionality
    Because the unit tests do not runon Android, create an example where we
    can test Android's filesystem behaviour, in comparison to the same tests
    run on other platforms.
    
    Include example assets, in the form of a simple text file.
    
    Change-Id: I7f700ce39115fb82c021b89c2a8712e2949b1797

---
+++ examples/filesystem/android/app/build.gradle.kts
@@ -0,0 +1,75 @@
+/*
+  This file is part of KDUtils.
+
+  SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.kdab.filesystem"
+    compileSdk = 34
+
+    defaultConfig {
+        ndk {
+            abiFilters += mutableSetOf("arm64-v8a")
+        }
+        applicationId = "com.kdab.filesystem"
+        minSdk = 32
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        prefab = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    ndkVersion = "26.1.10909125"
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.games:games-activity:1.2.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+// Task to copy files from outer project to android asset folders
+
+// Task to copy text files
+tasks.register<Copy>("copyText") {
+    from("../../../../assets/text")
+    into("src/main/assets/text")
+}
+
+// Ensure the copy task runs before the build task
+tasks.named("preBuild") {
+    dependsOn("copyText")
+}
